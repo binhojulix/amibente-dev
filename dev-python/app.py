45e31c5705e28306244b13eae9edfabbf5c47b03
binhojulix <fabio.jdluz@gmail.com>
2023-05-13 17:41:28 -0300

    configuracao

--- dev-python/app.py
+++ dev-python/app.py
@@ -1,12 +1,22 @@
 # -*- coding: utf-8 -*-
-from main.model.role import Role
-from main.model.user import User
 from flask import Flask
 from flask_cors import CORS
 import logging
 from config import app_config, app_active
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+import os
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.flaskenv')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+COV = None
+if os.environ.get('FLASK_COVERAGE'):
+    import coverage
+    COV = coverage.coverage(branch=True, include='app/*')
+    COV.start()
 
 config = app_config[app_active]
 
@@ -20,7 +30,6 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     #db = SQLAlchemy(config.APP)
    # migrate = Migrate(app, db)
-
    # db.init_app(app)
 
     # @app.shell_context_processor
