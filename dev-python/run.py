241c508b2148ba0a03ba309eb6149c4f9632f6bb
binhojulix <fabio.jdluz@gmail.com>
2023-05-14 00:03:33 -0300

    ajustes

--- dev-python/run.py
+++ dev-python/run.py
@@ -1,12 +1,9 @@
-from __future__ import absolute_import
-from importlib import reload
-import sys
-from config import app_config, app_active
-from app import create_app
+from flask import Flask
 
-config = app_config[app_active]
-config.APP = create_app()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    config.APP.run(host=config.IP_HOST, port=config.PORT_HOST)
-    reload(sys)
+@app.route('/')
+def index():
+    return 'Web App with Python Flask!'
+
+app.run(host='0.0.0.0', port=5000)
